aa72059272e8504a0c35f95aa7d800fe34a46c34
Lucho Pagano <lucho.pagano@gmail.com>
2019-09-12 19:17:10 -0300

    primero commit

---
+++ clasificados30/src/ttps/clasificados/Login.java
@@ -0,0 +1,45 @@
+package ttps.clasificados;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Login() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// read form fields
+        String username = request.getParameter("usuario");
+        String password = request.getParameter("pass");
+         
+        System.out.println("username: " + username);
+        System.out.println("password: " + password);
+	}
+
+}
